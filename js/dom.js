8470bc173c283e5817c0974603567deb85e82b5e
MD-ARAFATH <mdarafath2500@gmail.com>
2024-05-02 10:31:04 +0600

    secound commit

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,22 @@
 console.log("Hello world");
 console.log(document.body);
+
+//option-1 ---getElementByTagName
 const licollecttion = document.getElementsByTagName("li");
 console.log(licollecttion);
+
+//option-2 ---getElementById(using many tiimes)
 const fruit = document.getElementById("fruit-titles");
 console.log(fruit.innerText);
 
+//option-3 --getElemetsByClassName
 const places = document.getElementsByClassName("important-place");
 for (const place of places) {
   console.log(place.innerText);
 }
+
+//option-4 --quarySelector (will return the first One)
+
+//option-5 --quarySelectorAll(collecting the node list)
+const items = document.querySelectorAll(".fruits-container li");
+console.log(items);
